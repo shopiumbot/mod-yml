8d1b27d040b88f86288a0a187c6b3eaa9ab6acde
Andrew <andrew.panix@gmail.com>
2020-06-12 20:25:28 +0300

    init

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -4,20 +4,38 @@ namespace shopium\mod\yml\controllers;
 
 use shopium\mod\yml\components\YML;
 use Yii;
-use yii\web\Controller;
-use yii\web\Response;
+use core\components\controllers\AdminController;
+use shopium\mod\yml\models\SettingsForm;
 
-class DefaultController extends Controller {
+class DefaultController extends AdminController {
 
     public function actionIndex() {
+
+        $this->pageName = Yii::t('yml/default', 'MODULE_NAME');
+
+        $this->breadcrumbs[] = [
+            'label' => Yii::t('shop/default', 'MODULE_NAME'),
+            'url' => ['/admin/shop']
+        ];
+        $this->breadcrumbs[] = $this->pageName;
+        $model = new SettingsForm;
+
+        if ($model->load(Yii::$app->request->post())) {
+            if ($model->validate()) {
+                $model->save();
+            }
+            $this->refresh();
+        }
+        return $this->render('index', ['model' => $model]);
+    }
+    public function actionProcess() {
 
         //Yii::$app->log->targets['file1']->enabled = false;
         //Yii::$app->log->targets['file2']->enabled = false;
         //Yii::$app->log->targets['file3']->enabled = false;
         //Yii::$app->log->targets['file4']->enabled = false;
         //Yii::$app->response->format = Response::FORMAT_XML;
-        $xml = new YML;
+        $xml = new YML();
         $xml->processRequest();
     }
-
 }
